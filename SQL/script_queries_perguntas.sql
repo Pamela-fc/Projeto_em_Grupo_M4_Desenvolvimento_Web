fdbd31f2e55884475785203767993a879da419ce
Pâmela <115364351+Pamela-fc@users.noreply.github.com>
2023-03-09 10:13:02 -0300

    Update script_queries_perguntas.sql

--- SQL/script_queries_perguntas.sql
+++ SQL/script_queries_perguntas.sql
@@ -6,11 +6,13 @@ select season, episode, users_reviews from got_episodes_v4 order by users_review
 -- 2) Quais temporadas e episódios foram dirigidos por Alan Taylor?
 select season, episode from got_episodes_v4 where director = 'Alan Taylor';
 
--- 3) Quais casas estão localizadas no Norte (North)?
-select house_name from houses_v1 where region = 'north';
+-- 3) Quantas casas estão localizadas no Norte e nas Sub-regiões?
+select count(region) from houses_v1 where region like 'north';
+select count(region) from houses_v1 where region like 'north (wolfswood clans)';
+select count(region) from houses_v1 where region like 'north (crannogmen)';
 
 -- 4) Quantas familias tem a série e quantas são das Ilhas de Ferro (Iron Islands)?
-SELECT count(house_name) FROM houses_v1;
+select count(house_name) from houses_v1;
 select count(region) from houses_v1 where region like 'iron islands%';
 
 -- 5) Qual temporada (season) tem mais episódios com nota acima de 9? 
